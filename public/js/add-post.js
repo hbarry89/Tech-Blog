bf8c82f35dfa5eb476a65c409e23dfe255c78cec
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-03-08 09:12:45 -0500

    edit add post js

--- public/js/add-post.js
+++ public/js/add-post.js
@@ -1,8 +1,14 @@
 // alert("hello");
-
 const addPostForm = document.querySelector('#add-post');
+const titleEl = document.querySelector('#title');
+const bodyEl = document.querySelector('#body');
 
 addPostForm.addEventListener('submit', function(e) {
     e.preventDefault();
-    alert("hello");
+    // alert("hello");
+    const postInfo = {
+        title: titleEl.value,
+        body: bodyEl.value,
+    }
+    console.log(postInfo);
 });

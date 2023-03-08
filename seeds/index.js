fbdcbf44d764ab8dc7b3329bf79bd7664c1e25ff
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-03-07 20:41:57 -0500

    add seeds and edit models to reflect seed fields

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,23 @@
 const sequelize = require('../config/config');
 const Post = require('../models/Post');
 const postData = require('./post-seeds.json');
+const User = require('../models/User');
+const userData = require('./user-seeds.json');
+const Comment = require('../models/Comment');
+const commentData = require('./comment-seeds.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  })
   await Post.bulkCreate(postData, {
+    individualHooks: true,
+    returning: true,
+  })
+  await Comment.bulkCreate(commentData, {
     individualHooks: true,
     returning: true,
   });

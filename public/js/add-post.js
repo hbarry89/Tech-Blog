5cd7127505ce8f0d6ffe5e6052e9f4dd627669aa
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-03-07 21:56:06 -0500

    edit frontend js

--- public/js/add-post.js
+++ public/js/add-post.js
@@ -1,1 +1,8 @@
-alert("hello");
+// alert("hello");
+
+const addPostForm = document.querySelector('#add-post');
+
+addPostForm.addEventListener('click', function(e) {
+    e.preventDefault();
+    alert("hello");
+});
